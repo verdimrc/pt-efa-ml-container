80c41534aa1ef6fd25fd86c6f1cd909fc0d129f6
Verdi March <verdimrc@users.noreply.github.com>
2023-02-06 08:43:50 +0000

    Update build time

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+#
 docker system prune -f
 
 set -x
 
-/usr/bin/time docker build . -t pt-apex:std -f Dockerfile.apex-std | tee build-pt-apex-std.txt
+# Disable color in docker logs
+export NO_COLOR=1
+export BUILDKIT_PROGRESS=plain
+
+/usr/bin/time docker build . -t pt-apex:std -f Dockerfile.apex-std 2>&1 | tee build-pt-apex-std.txt
 sync ; sync
 
 sleep 30
-/usr/bin/time docker build . -t pt-apex:opt -f Dockerfile.apex-opt | tee build-pt-apex-opt.txt
+/usr/bin/time docker build . -t pt-apex:opt -f Dockerfile.apex-opt 2>&1 | tee build-pt-apex-opt.txt
 sync ; sync
 
 sleep 30
